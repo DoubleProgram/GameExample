5d875f92efca835c08e1e669df5303b29d7cba7f
DoubleProgram <92651727+DoubleProgram@users.noreply.github.com>
2022-08-08 17:40:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "External.h"
-
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+#include "Game.h"
 
 int main(int argc, char *argv[]) {
-//	ShowsCursor(false);
-//	SetWindowSize(70,20);
-//	cPrint(5,5,'S');
-	Setup(50,20);
-	getch(); //NOTE
-	return 0;
+    Setup(50,20);
+    Play();
+    getch();
+    return 0;
 }
