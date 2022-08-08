faec6344a07c2b8b62d39c3e8f839dcd827af35f
DoubleProgram <92651727+DoubleProgram@users.noreply.github.com>
2022-08-08 17:41:03 +0200

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,13 +1,18 @@
-#ifndef GAME_H_INCLUDED
-#define GAME_H_INCLUDED
+#ifndef Game_h
+#define Game_h
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "External.h"
 #include <time.h>
 
 typedef struct POS{
-	int8_t y;
-	int8_t x;
+    int8_t y;
+    int8_t x;
 }POS;
 
+void Setup(int width, int height);
+void Play();
 void Move();
 bool CanMove();
 void SpawnCoin();
